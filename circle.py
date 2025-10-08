2d2c9056b99138fcc4156b2e407586c6421c56fe
Daniil Ulyanov <smdfuck@icloud.com>
2025-10-08 21:07:48 +0300

    feat: add documentation for circle functions

--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
+    '''Принимает число r(радиус), возвращает площадь'''
     return math.pi * r * r
 
 
 def perimeter(r):
+    '''Принимает число r(радиус), возвращает периметр'''
     return 2 * math.pi * r
 
