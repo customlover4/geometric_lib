ddd15fa83be5a4c5155a0724fef0a7ddb2f1dbbd
Daniil Ulyanov <smdfuck@icloud.com>
2025-10-08 21:08:41 +0300

    feat: add documentation for triangle.py

--- triangle.py
+++ triangle.py
@@ -1,5 +1,7 @@
 def area(a, h):
+    '''Принимает числа a и h(сторона, высота), возвращает площадь'''
     return a * h / 2
 
-def perimeter(a ,b,c):
+def perimeter(a, b, c):
+    '''Принимает числа a, b, c(стороны), возвращает периметр'''
     return a + b + c
